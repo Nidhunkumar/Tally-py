ddc0cf6af8ad594ee7f1b518de2d6cd63331acbc
Nidhunkumar <you@domain.com>
2022-05-14 20:41:28 +0530

    third

--- window.py
+++ window.py
@@ -3,8 +3,9 @@ import tkinter as tk
 from tkinter import messagebox
 from PIL import Image,ImageTk
 from tkinter.ttk import Combobox
-
 from tkinter import ttk
+import mysql.connector
+
 
 hfont="-family {Segoe UI} -size 12 -weight bold "
 
@@ -94,11 +95,11 @@ Canvas3 = tk.Canvas(background="#e6ffff", insertbackground="black", relief="ridg
 Canvas3.place(relx=0.850, rely=0.07, relheight=0.8, relwidth=0.150)
 global f3
 f3=Frame(Canvas2,bg="white",relief=RIDGE,bd=1)
-f3.place(x=410,y=10 ,anchor="center")
+f3.pack()
 
 global sgaf1
-sgaf1=Frame(Canvas2,bg="white",relief=RIDGE,bd=5)
-sgaf1.place(x=330,y=110,anchor="center")
+sgaf1=Frame(Canvas2,bg="white",relief=RIDGE,bd=1)
+sgaf1.pack(pady=10)
 
 def Stock_group_analysis():
 
@@ -109,20 +110,20 @@ def Stock_group_analysis():
         value_inside = StringVar(Canvas2)
         value_inside.set("Select an Option")
         cmb=ttk.Combobox(f3,values=options_list,font=("times new roman",10,"bold"))
-        cmb.pack(fill=X,pady=2,padx=2)
+        cmb.pack(fill=X,pady=10,padx=10)
 
         
 
         f1_l0=Label(sgaf1,text="List of Stock groups",font=("times new roman",12,"bold"),bg="blue",fg="white")
-        f1_l0.pack(fill=X,pady=2,padx=2)
-        bt0=Button(sgaf1,text="group1",font=("times new roman",12,"bold"),bg="white",fg="red",command=selected_groups)
-        bt0.pack(fill=X,pady=2,padx=2)
+        f1_l0.pack(fill=X,pady=10,padx=10)
+        bt0=Button(sgaf1,text="group1sggqidqivds",font=("times new roman",12,"bold"),bg="white",fg="red",command=selected_groups)
+        bt0.pack(fill=X,pady=10,padx=10)
         bt1=Button(sgaf1,text="  group2",font=("times new roman",12,"bold"),bg="white",fg="red")
-        bt1.pack(fill=X,pady=2,padx=2)
+        bt1.pack(fill=X,pady=10,padx=10)
         bt2=Button(sgaf1,text="group3",font=("times new roman",12,"bold"),bg="white",fg="red")
-        bt2.pack(fill=X,pady=2,padx=2)
+        bt2.pack(fill=X,pady=10,padx=10)
         bt3=Button(sgaf1,text="group4",font=("times new roman",12,"bold"),bg="white",fg="red")
-        bt3.pack(fill=X,pady=2,padx=2)
+        bt3.pack(fill=X,pady=10,padx=10)
 
 
 
@@ -130,13 +131,8 @@ def selected_groups():
         f1.destroy()
         f3.destroy()
         sgaf1.destroy()
-        l1=Label(Canvas1,text="Stock group analysis",font=("times new roman",13,"bold"),bg="blue",fg="white",relief=GROOVE,bd=5)
-        l1.grid(row=0,column=0,ipadx=35,)
-
-        options_list=["Period","Stock Category Analysis","Stock Item Analysis"]
-        cm=ttk.Combobox(f0 ,values=options_list,font=("times new roman",10,"bold"))
-        cm.pack(fill=X,pady=2,padx=2)
-        cm.current(0)
+        Canvas2.destroy()
+        global selected_groups_frame
         selected_groups_frame=Frame(Canvas1,bg="white",relief=RAISED,bd=2)
         selected_groups_frame.place(x=10,y=35,width=1300,height=650)
 
@@ -155,7 +151,7 @@ def selected_groups():
 
         f13=Frame(selected_groups_frame,bg="white",relief=RAISED,bd=2)
         f13.place(x=0,y=145,width=607,height=450)
-        f13bt1=Button(f13,text="PRODUCT-1",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+        f13bt1=Button(f13,text="PRODUCT-1",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0,command=movement_values)
         f13bt1.place(x=0,y=0,anchor="nw")
         f13bt2=Button(f13,text="PRODUCT-2",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
         f13bt2.place(x=0,y=30,anchor="nw")
@@ -223,9 +219,370 @@ def selected_groups():
         f20.place(x=950,y=598,width=340,height=48)
         l7f6=Label(f20,text="100",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
         l7f6.place(x=0,y=0,anchor="nw")
+def movement_values():
+    
+
+    global movement_frame
+    movement_frame=Frame(Canvas1,bg="white",relief=RAISED,bd=2)
+    movement_frame.place(x=10,y=35,width=1300,height=650)
+
+    f11=Frame(movement_frame,bg="white",relief=RAISED,bd=2)
+    f11.grid(row=1,column=0,columnspan=3,ipadx=200)
+    l1f1=Label(f11,text="Perticulars",font=("times new roman",12,"bold"),bg="white",fg="black",width=33,height=7)
+    l1f1.pack(fill=X)
+    f12=Frame(movement_frame,bg="white",relief=RAISED,bd=2)
+    f12.place(x=710,y=0,width=580,height=80)
+    l1f2=Label(f12,text="PRODUCT NAME",font=("times new roman",9,"bold"),bg="white",fg="black",borderwidth=5)
+    l1f2.place(x=305,y=10,anchor="center")
+    l1f3=Label(f12,text="C NAME",font=("times new roman",9,"bold"),bg="white",fg="black")
+    l1f3.place(x=305,y=30,anchor="center")
+    l1f4=Label(f12,text="FOR 1-APR-20",font=("times new roman",9,"bold"),bg="white",fg="black")
+    l1f4.place(x=305,y=50,anchor="center")
+
+    f13=Frame(movement_frame,bg="white",relief=RAISED,bd=2)
+    f13.place(x=0,y=150,width=710,height=450)
+    f13bt1=Label(f13,text="Movement inward",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    f13bt1.place(x=0,y=0,anchor="nw")
+    f13bt2=Label(f13,text="Suppliers",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    f13bt2.place(x=10,y=30,anchor="nw")
+    f13bt3=Button(f13,text="C-name",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0,command=item_values)
+    f13bt3.place(x=20,y=60,anchor="nw")
+    f13bt4=Button(f13,text="Movement outwards",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    f13bt4.place(x=0,y=130,anchor="nw")
+    f13bt5=Button(f13,text="Buyers",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    f13bt5.place(x=10,y=160,anchor="nw")
+    f13bt6=Button(f13,text="C-name",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    f13bt6.place(x=20,y=190,anchor="nw")
 
 
-            
+
+
+    f14=Frame(movement_frame,bg="white",relief=RAISED,bd=1)
+    f14.place(x=710,y=83,width=580,height=58)
+    l1f5=Label(f14,text="Movement values",font=("times new roman",9,"bold"),bg="white",fg="black",borderwidth=5)
+    l1f5.place(x=0,y=0,anchor="nw")
+    l1f6=Label(f14,text="Quantity",font=("times new roman",9,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f6.place(x=0,y=30,anchor="nw")
+    l1f7=Label(f14,text="Basic Rate",font=("times new roman",9,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f7.place(x=150,y=30,anchor="nw")
+    l1f8=Label(f14,text="Effective rate",font=("times new roman",9,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f8.place(x=260,y=30,anchor="nw")
+    l1f9=Label(f14,text="Values",font=("times new roman",9,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f9.place(x=320,y=30,anchor="nw")
+
+    f15=Frame(movement_frame,bg="white",relief=RAISED,bd=1)
+    f15.place(x=710,y=150,width=580,height=450)
+
+    l4f6=Label(f15,text="10 Nos",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f6.place(x=0,y=60,anchor="nw")
+    l4f8=Label(f15,text="10",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f8.place(x=150,y=60,anchor="nw")
+    l4f7=Label(f15,text="10",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f7.place(x=260,y=60,anchor="nw")
+    l4f8=Label(f15,text="100",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f8.place(x=320,y=60,anchor="nw")
+    
+    horizontal =Frame(f15, bg='black', height=1,width=360)
+    horizontal.place(x=0, y=98)
+    horizontal =Frame(f15, bg='black', height=1,width=360)
+    horizontal.place(x=0, y=122)
+
+    l4f6=Label(f15,text="10 Nos",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f6.place(x=0,y=100,anchor="nw")
+    l4f8=Label(f15,text="10",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f8.place(x=150,y=100,anchor="nw")
+    l4f7=Label(f15,text="10",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f7.place(x=260,y=100,anchor="nw")
+    l4f8=Label(f15,text="100",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f8.place(x=320,y=100,anchor="nw")
+
+    l4f6=Label(f15,text="10 Nos",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f6.place(x=0,y=190,anchor="nw")
+    l4f8=Label(f15,text="10",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f8.place(x=150,y=190,anchor="nw")
+    l4f7=Label(f15,text="10",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f7.place(x=260,y=190,anchor="nw")
+    l4f8=Label(f15,text="100",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f8.place(x=320,y=190,anchor="nw")
+
+    l4f6=Label(f15,text="10 Nos",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f6.place(x=0,y=210,anchor="nw")
+    l4f8=Label(f15,text="10",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f8.place(x=150,y=210,anchor="nw")
+    l4f7=Label(f15,text="10",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f7.place(x=260,y=210,anchor="nw")
+    l4f8=Label(f15,text="100",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f8.place(x=320,y=210,anchor="nw")
+
+    horizontal =Frame(f15, bg='black', height=1,width=360)
+    horizontal.place(x=0, y=210)
+    horizontal =Frame(f15, bg='black', height=1,width=360)
+    horizontal.place(x=0, y=230)
+
+# data = [
+# 	["John", "Elder", 1, "123 Elder St.", "Las Vegas", "NV", "89137"],
+# 	["Mary", "Smith", 2, "435 West Lookout", "Chicago", "IL", "60610"],
+# 	["Tim", "Tanaka", 3, "246 Main St.", "New York", "NY", "12345"],
+# 	["Erin", "Erinton", 4, "333 Top Way.", "Los Angeles", "CA", "90210"],
+# 	["Bob", "Bobberly", 5, "876 Left St.", "Memphis", "TN", "34321"],
+# 	["Steve", "Smith", 6, "1234 Main St.", "Miami", "FL", "12321"],
+# 	["Tina", "Browne", 7, "654 Street Ave.", "Chicago", "IL", "60611"],
+# 	["Mark", "Lane", 8, "12 East St.", "Nashville", "TN", "54345"],
+# 	["John", "Smith", 9, "678 North Ave.", "St. Louis", "MO", "67821"],
+# 	["Mary", "Todd", 10, "9 Elder Way.", "Dallas", "TX", "88948"],
+# 	["John", "Lincoln", 11, "123 Elder St.", "Las Vegas", "NV", "89137"],
+# 	["Mary", "Bush", 12, "435 West Lookout", "Chicago", "IL", "60610"],
+# 	["Tim", "Reagan", 13, "246 Main St.", "New York", "NY", "12345"],
+# 	["Erin", "Smith", 14, "333 Top Way.", "Los Angeles", "CA", "90210"],
+# 	["Bob", "Field", 15, "876 Left St.", "Memphis", "TN", "34321"],
+# 	["Steve", "Target", 16, "1234 Main St.", "Miami", "FL", "12321"],
+# 	["Tina", "Walton", 17, "654 Street Ave.", "Chicago", "IL", "60611"],
+# 	["Mark", "Erendale", 18, "12 East St.", "Nashville", "TN", "54345"],
+# 	["John", "Nowerton", 19, "678 North Ave.", "St. Louis", "MO", "67821"],
+# 	["Mary", "Hornblower", 20, "9 Elder Way.", "Dallas", "TX", "88948"]
+	
+# ]
+def item_values():
+    
+    global item_nm
+    item_nm=Frame(Canvas1,height=200,width=1308,bg="white",relief=RAISED,bd=2)
+
+    item_nm.place(x=0,y=21)
+    lb1=Label(item_nm,text="Stock Item:",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    lb1.place(x=0,y=0,anchor="nw")
+    lb2=Label(item_nm,text="Item Name",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    lb2.place(x=80,y=0,anchor="nw")
+    lb3=Label(item_nm,text="Inwards under ledger:",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    lb3.place(x=0,y=20,anchor="nw")
+    lb4=Label(item_nm,text="Item Name",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    lb4.place(x=150,y=20,anchor="nw")
+    lb5=Label(item_nm,text="for apr-10-22",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    lb5.place(x=1200,y=0,anchor="nw")
+    
+
+
+
+    global item_val
+    item_val=Frame(Canvas1,bg="white",relief=RAISED,bd=2)
+    item_val.place(x=0,y=70,width=1308,height=650)
+    global tree
+    tree = ttk.Treeview(item_val, column=("c1", "c2", "c3","c4", "c5", "c6","c7", "c8"), show='headings', height=650)
+
+    tree.column("#1", anchor=tk.CENTER,width=50)
+
+    tree.heading("#1", text="Date")
+
+    tree.column("#2", anchor=tk.CENTER,width=830)
+
+    tree.heading("#2", text="Perticulars")
+
+    tree.column("#3", anchor=tk.CENTER,width=70)
+
+    tree.heading("#3", text="Quantity")
+
+    tree.column("#4", anchor=tk.CENTER,width=70)
+
+    tree.heading("#4", text="Basic Rate")
+
+    tree.column("#5", anchor=tk.CENTER,width=70)
+
+    tree.heading("#5", text="Basic value")
+
+    tree.column("#6", anchor=tk.CENTER,width=70)
+
+    tree.heading("#6", text="Added cost")
+
+    tree.column("#7", anchor=tk.CENTER,width=70)
+
+    tree.heading("#7", text="Total value")
+
+    tree.column("#8", anchor=tk.CENTER,width=70)
+
+    tree.heading("#8", text="Eff.rate")
+    tree.pack()
+    tree.bind("<Double-1>",OnDoubleClick)
+    
+
+
+
+
+    horizontal =Frame(item_val, bg='white', height=100,width=1308)
+    horizontal.place(x=0, y=520)
+    horizontal1 =Frame(item_val, bg='black', height=1,width=1308)
+    horizontal1.place(x=0, y=520)
+    horizontal1 =Frame(item_val, bg='black', height=1,width=1308)
+    horizontal1.place(x=0, y=545)
+    l4f6=Label(horizontal,text="Total",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f6.place(x=0,y=5,anchor="nw")
+
+    l4f11=Label(horizontal,text="10 Nos",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f11.place(x=900,y=5,anchor="nw")
+    l4f10=Label(horizontal,text="10",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f10.place(x=1000,y=5,anchor="nw")
+    l4f9=Label(horizontal,text="10",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f9.place(x=1060,y=5,anchor="nw")
+    l4f8=Label(horizontal,text="100",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f8.place(x=1130,y=5,anchor="nw")
+    l4f7=Label(horizontal,text="1000",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f7.place(x=1190,y=5,anchor="nw")
+    l4f8=Label(horizontal,text="10000",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    l4f8.place(x=1250,y=5,anchor="nw")
+
+    
+
+    conn = mysql.connector.connect(host ="localhost",
+                                     user = "root",
+                                     password ="",
+                                     db ="tally")
+    c= conn.cursor()
+
+    
+   
+
+    
+    # c.execute("""CREATE TABLE if not exists stock (
+    #     first_name text,
+    #     last_name text,
+    #     id integer,
+    #     address text,
+    #     city text,
+    #     state text,
+    #     zipcode text)
+    #     """)
+   
+    
+    # for record in data:
+    #     c.execute("INSERT INTO stock VALUES (%s, %s, %s, %s, %s, %s, %s)", record)
+    # conn.commit()
+    c.execute("SELECT * FROM stock")
+    records = c.fetchall()
+
+    for record in records:
+        tree.insert("", "end", values=record)
+    
+        
+
+     
+
+
+
+    
+    # f16=Frame(movement_frame,bg="white",relief=RAISED,bd=2)
+    # f16.place(x=610,y=145,width=340,height=450)
+    # l3f6=Label(f16,text="10 Nos",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    # l3f6.place(x=0,y=0,anchor="nw")
+    # l3f7=Label(f16,text="10",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    # l3f7.place(x=80,y=0,anchor="nw")
+    # l3f8=Label(f16,text="100",font=("times new roman",12,"bold"),bg="white",fg="black",borderwidth=0)
+    # l3f8.place(x=180,y=0,anchor="nw")
+    
+def OnDoubleClick(e):
+    f1.destroy()
+    f3.destroy()
+    sgaf1.destroy()
+    Canvas2.destroy()
+    item_nm.destroy()
+    item_val.destroy()
+    global bill_frame
+    bill_frame=Frame(Canvas1,bg="white",relief=RAISED,bd=2)
+    bill_frame.place(x=0,y=0,width=1308,height=200)
+    l1f1=Label(bill_frame,text="Purchase No:",font=("times new roman",11,"bold"),bg="#00254a",fg="white",borderwidth=0)
+    l1f1.place(x=0,y=0)
+    l1f2=Label(bill_frame,text="1",font=("times new roman",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f2.place(x=125,y=0)
+    l1f3=Label(bill_frame,text="Supplier invoice no:",font=("times new roman",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f3.place(x=0,y=30)
+    l1f4=Label(bill_frame,text="1",font=("times new roman",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f4.place(x=135,y=30)
+    l1f5=Label(bill_frame,text="Party A/c name:",font=("times new roman",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f5.place(x=0,y=60)
+    l1f6=Label(bill_frame,text="abc",font=("times new roman",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f6.place(x=125,y=60)
+    l1f7=Label(bill_frame,text="Current balance:",font=("times new roman",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f7.place(x=0,y=90)
+    l1f8=Label(bill_frame,text="10000cr",font=("times new roman",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f8.place(x=125,y=90)
+    l1f9=Label(bill_frame,text="Purchase ledger:",font=("times new roman",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f9.place(x=0,y=120)
+    l1f10=Label(bill_frame,text="Purchase acc",font=("times new roman",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f10.place(x=125,y=120)
+    l1f11=Label(bill_frame,text="Current balance:",font=("times new roman",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f11.place(x=0,y=150)
+    l1f12=Label(bill_frame,text="10000cr",font=("times new roman",11,"bold"),bg="white",fg="black",borderwidth=0)
+    l1f12.place(x=125,y=150)
+    horizontal1 =Frame(bill_frame, bg='black', height=1,width=1308)
+    horizontal1.place(x=0, y=175)
+
+    bill_tbl=Frame(Canvas1,bg="white",relief=RAISED,bd=2)
+    bill_tbl.place(x=0,y=180,width=1308,height=650)
+
+
+    global tree
+    tree = ttk.Treeview(bill_tbl, column=("c1", "c2", "c3","c4"), show='headings',height=650)
+
+    tree.column("#1", anchor=tk.W,width=830)
+
+    tree.heading("#1", text="Item Name",anchor=tk.W)
+
+    tree.column("#2", anchor=tk.CENTER,width=70)
+
+    tree.heading("#2", text="Quantity")
+
+    tree.column("#3", anchor=tk.CENTER,width=70)
+
+    tree.heading("#3", text="Rate")
+
+    tree.column("#4", anchor=tk.CENTER,width=70)
+
+    tree.heading("#4", text="Amount")
+
+    tree.pack(fill=tk.BOTH)
+
+    conn = mysql.connector.connect(host ="localhost",
+                                     user = "root",
+                                     password ="",
+                                     db ="tally")
+    c= conn.cursor()
+
+    
+   
+
+    
+    # c.execute("""CREATE TABLE if not exists stock (
+    #     first_name text,
+    #     last_name text,
+    #     id integer,
+    #     address text,
+    #     city text,
+    #     state text,
+    #     zipcode text)
+    #     """)
+   
+    
+    # for record in data:
+    #     c.execute("INSERT INTO stock VALUES (%s, %s, %s, %s, %s, %s, %s)", record)
+    # conn.commit()
+    c.execute("SELECT * FROM stock")
+    records = c.fetchall()
+
+    for record in records:
+        tree.insert("", "end", values=record)
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 global f1
 f1=Frame(Canvas2,bg="white",relief=RAISED,bd=1)
